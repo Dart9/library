244cb215a88297fc6df6e9b01c218099604d8920
DartDart72 <david.regnault.cherbourg@gmail.com>
2020-12-28 19:28:11 +0100

    Put @Data on LoanDTO class

--- src/main/java/com/dartnine/library/loan/LoanDTO.java
+++ src/main/java/com/dartnine/library/loan/LoanDTO.java
@@ -2,9 +2,11 @@ package com.dartnine.library.loan;
 
 import com.dartnine.library.book.Book;
 import com.dartnine.library.customer.Customer;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class LoanDTO {
     private Customer customerDTO;
     private LocalDate beginDate;
@@ -12,52 +14,4 @@ public class LoanDTO {
     private LocalDate loanBeginDate;
     private LocalDate loanEndDate;
     private Book bookDTO;
-
-    public Customer getCustomerDTO() {
-        return customerDTO;
-    }
-
-    public void setCustomerDTO(Customer customerDTO) {
-        this.customerDTO = customerDTO;
-    }
-
-    public LocalDate getBeginDate() {
-        return beginDate;
-    }
-
-    public void setBeginDate(LocalDate beginDate) {
-        this.beginDate = beginDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public LocalDate getLoanBeginDate() {
-        return loanBeginDate;
-    }
-
-    public void setLoanBeginDate(LocalDate loanBeginDate) {
-        this.loanBeginDate = loanBeginDate;
-    }
-
-    public LocalDate getLoanEndDate() {
-        return loanEndDate;
-    }
-
-    public void setLoanEndDate(LocalDate loanEndDate) {
-        this.loanEndDate = loanEndDate;
-    }
-
-    public Book getBookDTO() {
-        return bookDTO;
-    }
-
-    public void setBookDTO(Book bookDTO) {
-        this.bookDTO = bookDTO;
-    }
 }
